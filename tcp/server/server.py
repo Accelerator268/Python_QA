e1630ede3bb5798b10c6dd545b3ecc69bccc5255
OVERGEARED <103391825+Accelerator268@users.noreply.github.com>
2025-06-17 01:28:50 +0300

    Добавлены комментарии в server.py

--- tcp/server/server.py
+++ tcp/server/server.py
@@ -3,28 +3,36 @@ import os
 
 
 def tcp_server() -> None:
+    #Определение названия хоста и порта, на котором сервер будет принимать сигналы
     host: str = 'localhost'
     port: int = 20000
 
+    #Устанавливаем, в какой папке будут происходить последующие процессы
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+    #Создание сервера
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
         server.bind((host, port))
         server.listen(1)
         print(f"TCP Server listening on {host}:{port}")
 
+        #Запуск сервера
         while True:
             conn, address = server.accept()
             address = ':'.join([str(i) for i in address])
             print(f"Request from {address}")
 
+            #Сервер проверяет полученную информацию
             try:
+                #Сервер получает имя файла
                 filename = conn.recv(1024).decode()
                 if not filename:
                     continue
 
+                #Сервер выводит название запрошенного файла, если полученная информация является названием файла
                 print(f"Client requested file: {filename}\nSending...")
 
+                #Если запрошенный файл существует, то сервер его отправляет. Если нет, то сообщает об этом
                 if os.path.exists(filename):
                     filesize: int = os.path.getsize(filename)
                     conn.send(str(filesize).encode())
@@ -40,12 +48,14 @@ def tcp_server() -> None:
                     conn.send(b"-1")
                     print(f"File not found\nFailed sending to {address}")
 
+            #Если возникла ошибка, то выводит ее
             except Exception as exception:
                 print(f"Error: {exception}")
 
+            #Сервер закрывает соединение с клиентом, но сам продолжает работу
             finally:
                 conn.close()
 
-
+#Основное тело программы
 if __name__ == "__main__":
     tcp_server()
