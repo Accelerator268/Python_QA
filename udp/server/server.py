6bfa3700a0b14fc2cdb12197a60983d2a48e3c0d
OVERGEARED <103391825+Accelerator268@users.noreply.github.com>
2025-06-17 02:04:17 +0300

    Добавлены комментарии для udp server.py

--- udp/server/server.py
+++ udp/server/server.py
@@ -3,28 +3,36 @@ import os
 
 
 def udp_server() -> None:
+    #Определение названия хоста и порта, на котором сервер будет принимать сигналы
     host: str = 'localhost'
     port: int = 20000
     buffer_size: int = 4096
     
+    #Устанавливаем, в какой папке будут происходить последующие процессы
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+    #Создание сервера
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server:
         server.bind((host, port))
         print(f"UDP Server listening on {host}:{port}")
         
+        #Запуск сервера
         while True:
             try:
+                #Сервер получает запрос с именем файла
                 data, address = server.recvfrom(buffer_size)
                 filename: str = data.decode()
                 print(f"Client {address} requested file: {filename}")
                 
                 if os.path.exists(filename):
+                    #Сервер выводит название запрошенного файла, если полученная информация является названием файла
                     print(f"Client requested file: {filename}\nSending...")
 
                     filesize: int = os.path.getsize(filename)
+                    # Отправляем размер файла
                     server.sendto(str(filesize).encode(), address)
                     
+                    # Отправляем файл по частям
                     with open(filename, 'rb') as file:
                         while True:
                             chunk: bytes = file.read(buffer_size)
@@ -39,6 +47,6 @@ def udp_server() -> None:
             except Exception as exception:
                 print(f"Error: {exception}")
 
-
+#Основное тело программы
 if __name__ == "__main__":
     udp_server()
